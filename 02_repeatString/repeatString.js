075ca7730834e178b6f6ee27456af66537fff1e3
johncarlohexi <hexkundiman@gmail.com>
2023-07-05 22:17:26 +0900

    Repeat String Completed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+let string1 = "hello"
+const repeatString = function(rString, times) {
+    let output = ""
+    for (let i = 0; i < times; i++) {
+        output += rString
+    } return output
 };
+repeatString(string1, 2)
+console.log(repeatString(string1, 2))
 
 // Do not edit below this line
 module.exports = repeatString;
