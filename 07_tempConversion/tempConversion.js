91f469f8d95db9b599bf7541af04ac255a89a19e
johncarlohexi <hexkundiman@gmail.com>
2023-07-10 21:17:44 +0900

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let newTemp = (temp - 32) * 5/9
+  let roundOff = Math.round(newTemp)
+  return roundOff
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let newTemp = (temp * 1.8) + 32
+  let roundOff = Math.round(newTemp)
+  return roundOff
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
+let books = [{
+    title:1,
+    author: "carlo"
+}, {
+    title:2,
+    author:"kurt"
+} ]
 
+const getTheTitles = function(array) {
+    return array.map(book => book.title)
 };
+
+getTheTitles(books)
 
 // Do not edit below this line
 module.exports = getTheTitles;
